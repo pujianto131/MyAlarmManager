ae4ae88cc6a23de2b5c608d4623afc5a8e1f049f
pujianto131 <pujianto.e131@gmail.com>
2018-08-01 00:23:51 +0700

    Initial commit

---
+++ app/src/main/java/com/pujianto131/myalarmmanager/AlarmPreference.java
@@ -0,0 +1,69 @@
+package com.pujianto131.myalarmmanager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AlarmPreference {
+    private final String PREF_NAME = "AlarmPreference";
+    private final String KEY_ONE_TIME_DATE = "oneTimeDate";
+    private final String KEY_ONE_TIME_TIME = "oneTimeTime";
+    private final String KEY_ONE_TIME_MESSAGE = "oneTimeMessage";
+    private final String KEY_REPEATING_TIME = "repeatingTime";
+    private final String KEY_REPEATING_MESSAGE = "repeatingMessage";
+    private SharedPreferences mSharedPreference;
+    private SharedPreferences.Editor editor;
+
+    public AlarmPreference(Context context){
+        mSharedPreference = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = mSharedPreference.edit();
+    }
+
+    public void setOneTimeDate(String date){
+        editor.putString(KEY_ONE_TIME_DATE, date);
+        editor.commit();
+    }
+
+    public String getOneTimeDate(){
+        return mSharedPreference.getString(KEY_ONE_TIME_DATE, null);
+    }
+
+
+    public void setOneTimeTime(String time){
+        editor.putString(KEY_ONE_TIME_TIME, time);
+        editor.commit();
+    }
+    public String getOneTimeTime(){
+        return mSharedPreference.getString(KEY_ONE_TIME_TIME, null);
+    }
+
+    public void setOneTimeMessage(String message){
+        editor.putString(KEY_ONE_TIME_MESSAGE, message);
+        editor.commit();
+    }
+    public String getOneTimeMessage(){
+        return mSharedPreference.getString(KEY_ONE_TIME_MESSAGE, null);
+    }
+
+    public void setRepeatingTime(String time){
+        editor.putString(KEY_REPEATING_TIME, time);
+        editor.commit();
+    }
+    public String getOneRepeatingTime(){
+        return mSharedPreference.getString(KEY_REPEATING_TIME, null);
+    }
+
+    public void setOneRepeatingMesage(String mesage){
+        editor.putString(KEY_REPEATING_MESSAGE, mesage);
+        editor.commit();
+    }
+    public String getOneRepeatingMessage(){
+        return mSharedPreference.getString(KEY_REPEATING_MESSAGE,null);
+    }
+
+
+    public void clear(){
+        editor.clear();
+        editor.commit();
+    }
+
+}
---
+++ app/src/main/java/com/pujianto131/myalarmmanager/AlarmReceiver.java
@@ -0,0 +1,81 @@
+package com.pujianto131.myalarmmanager;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    public static final String TYPE_ONE_TIME = "OneTimeAlarm";
+    public static final String TYPE_REPEATING = "RepeatingAlarm";
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_TYPE = "type";
+
+    private final int NOTIF_ID_ONETIME = 100;
+    private final int NOTIF_ID_REPEATING = 101;
+
+    public AlarmReceiver(){
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String type = intent.getStringExtra(EXTRA_TYPE);
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        String title = type.equalsIgnoreCase(TYPE_ONE_TIME)? "One time Alarm" : "Repeating Alarm";
+        int notifId = type.equalsIgnoreCase(TYPE_ONE_TIME)? NOTIF_ID_ONETIME : NOTIF_ID_REPEATING;
+        showAlarmNotification(context, title, message, notifId);
+    }
+
+    private void showAlarmNotification(Context context, String title, String message, int notifId){
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_access_alarm_black_36dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+                .setVibrate(new  long[]{1000, 1000, 1000, 1000, 1000})
+                .setSound(alarmSound);
+        notificationManagerCompat.notify(notifId, builder.build());
+    }
+
+    public void setOneTimeAlarm(Context context, String type, String date, String time, String message){
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_TYPE, type);
+        String dateArray[] = date.split("-");
+        String timeArray[] = time.split(":");
+        Calendar kalender = Calendar.getInstance();
+        kalender.set(Calendar.YEAR, Integer.parseInt(dateArray[0]));
+        kalender.set(Calendar.MONTH, Integer.parseInt(dateArray[1])-1);
+        kalender.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateArray[2]));
+        kalender.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+        kalender.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+        kalender.set(Calendar.SECOND, 0);
+
+        int requestCode = NOTIF_ID_ONETIME;
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, kalender.getTimeInMillis(), pendingIntent);
+        Toast.makeText(context, "One time alarm set up", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelAlarm(Context context, String type){
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        int requestCode = type.equalsIgnoreCase(TYPE_ONE_TIME) ? NOTIF_ID_ONETIME : NOTIF_ID_REPEATING;
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0);
+        alarmManager.cancel(pendingIntent);
+    }
+}
